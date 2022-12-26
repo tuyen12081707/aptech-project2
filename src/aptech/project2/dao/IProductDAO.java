dc30bfb5d78718f8c3b3c0a6b6ee00756499c0d3
tuyen12081707 <105075028+tuyen12081707@users.noreply.github.com>
2022-12-26 23:37:25 +0700

    add order

--- src/aptech/project2/dao/IProductDAO.java
+++ src/aptech/project2/dao/IProductDAO.java
@@ -4,8 +4,6 @@ import aptech.project2.entities.Product;
 import java.util.List;
 
 public interface IProductDAO extends BaseDAO<Product>{
-        
-    
     
     
 }
---
+++ src/aptech/project2/dao/OrderDAO.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aptech.project2.dao;
+
+import aptech.project2.entities.Order;
+import java.util.List;
+
+/**
+ *
+ * @author Admin
+ */
+public class OrderDAO implements IOrderDAO{
+
+    @Override
+    public boolean create(Order object) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean update(Order object) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean deleteById(int id) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Order findById(int id) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Order> findAll() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
