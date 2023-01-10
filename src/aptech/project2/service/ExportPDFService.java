fb62f2e39854d9dbc5ad5137a75d15b1163fbdcd
taithanh95 <taithanh95.humg@gmail.com>
2023-01-10 22:55:31 +0700

    update export excel

--- src/aptech/project2/service/ExportPDFService.java
+++ src/aptech/project2/service/ExportPDFService.java
@@ -5,22 +5,20 @@
  */
 package aptech.project2.service;
 
-import aptech.project2.common.CommonException;
 import aptech.project2.common.FileCommon;
-import aptech.project2.constant.Constant;
+import aptech.project2.constant.Constant1;
 import aptech.project2.dao.Orders;
 import aptech.project2.jasper.JasperPrintService;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import net.sf.jasperreports.engine.JREmptyDataSource;
 import net.sf.jasperreports.engine.JRParameter;
 import net.sf.jasperreports.engine.JasperCompileManager;
@@ -30,6 +28,7 @@ import net.sf.jasperreports.engine.JasperReport;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import net.sf.jasperreports.engine.design.JasperDesign;
 import net.sf.jasperreports.engine.xml.JRXmlLoader;
+import org.apache.commons.io.FileUtils;
 
 /**
  *
@@ -49,26 +48,26 @@ public class ExportPDFService {
         JasperPrint jp;
         try {
             FileCommon fileUtils = new FileCommon();
-            File invoice = fileUtils.getFileFromResource(Constant.INVOICE_TEMPLATE_FILE);
+            File invoice = fileUtils.getFileFromResource(Constant1.INVOICE_TEMPLATE_FILE);
             System.out.println("logggggg: " + invoice.toString());
-            File bill = fileUtils.getFileFromResource(Constant.INVOICE_EXPORT_FILE);
+            File bill = fileUtils.getFileFromResource(Constant1.INVOICE_EXPORT_FILE);
             System.out.println("logggggg22: " + bill.toString());
             JRBeanCollectionDataSource itemsJRBean = new JRBeanCollectionDataSource(lst);
             Map<String, Object> parameters = new HashMap<>();
             parameters.put("ItemDataSource", itemsJRBean);
-            parameters.put("cashier", "");
-            parameters.put("customerName", ""); // order might not have customer
-            parameters.put("customerAddress", "");
-            parameters.put("customerPhone", "");
-            parameters.put("invoiceNumber", "");
+            parameters.put("cashier", "Thành");
+            parameters.put("customerName", "test"); 
+            parameters.put("customerAddress", "Hà Nội");
+            parameters.put("customerPhone", "0987654321");
+            parameters.put("invoiceNumber", 1);
             parameters.put(JRParameter.REPORT_LOCALE, new Locale("vi", "VN"));
 
             InputStream input = new FileInputStream(invoice);
             JasperDesign jasperDesign = JRXmlLoader.load(input);
             JasperReport jasperReport = JasperCompileManager.compileReport(jasperDesign);
             jp = JasperFillManager.fillReport(jasperReport, parameters, new JREmptyDataSource());
-//            FileUtils.writeByteArrayToFile(bill, jasperPrintService.exportReportXlsx(jp));
-//            file = bill.getAbsolutePath();
+            FileUtils.writeByteArrayToFile(bill, jasperPrintService.exportReportXlsx(jp));
+            file = bill.getAbsolutePath();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
@@ -77,10 +76,13 @@ public class ExportPDFService {
     public static void main(String[] args) {
         ExportPDFService ex = new ExportPDFService();
         Orders o = new Orders();
+        o.setId(1);
+        o.setAmount(new BigDecimal(1));
+        o.setData("abcd");
         try {
             ex.requestInvoicePdf(o);
         } catch (Exception ex1) {
-            Logger.getLogger(ExportPDFService.class.getName()).log(Level.SEVERE, null, ex1);
+            ex1.printStackTrace();
         }
     }
 }
